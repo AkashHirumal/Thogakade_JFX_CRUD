217113e0775a8610e28034b11516edb0ccaa3894
AkashHirumal <akashhirumal@gmail.com>
2025-01-01 13:22:36 +0530

    Customer Add Funtion

--- src/main/java/controller/CustomerFormController.java
+++ src/main/java/controller/CustomerFormController.java
@@ -4,14 +4,12 @@ import db.DBConnection;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.cell.PropertyValueFactory;
 import model.Customer;
 
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ResourceBundle;
 
 import com.jfoenix.controls.JFXTextField;
@@ -35,7 +33,7 @@ public class CustomerFormController implements Initializable {
     private TableColumn<?, ?> colSalary;
 
     @FXML
-    private TableView<?> tblCustomers;
+    private TableView<Customer> tblCustomers;
 
     @FXML
     private JFXTextField txtAddress;
@@ -51,6 +49,21 @@ public class CustomerFormController implements Initializable {
 
     @FXML
     void btnAddOnAction(ActionEvent event) {
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            String SQL = "INSERT INTO customer VALUES(?,?,?,?)";
+            PreparedStatement psTm = connection.prepareStatement(SQL);
+            psTm.setString(1,txtId.getText());
+            psTm.setString(2,txtName.getText());
+            psTm.setString(3,txtAddress.getText());
+            psTm.setDouble(4,Double.parseDouble(txtSalary.getText()));
+            if(psTm.executeUpdate()>0){
+                new Alert(Alert.AlertType.INFORMATION,"Added Customer !!").show();
+                loadTable();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
@@ -69,7 +82,14 @@ public class CustomerFormController implements Initializable {
 
     }
 
-    private void loadTable() throws SQLException {
+    private void setTextToValues(Customer customer) {
+        txtId.setText(customer.getId());
+        txtName.setText(customer.getName());
+        txtAddress.setText(customer.getAddress());
+        txtSalary.setText(String.valueOf(customer.getSalary()));
+    }
+
+    private void loadTable() {
 
 
         colId.setCellValueFactory(new PropertyValueFactory<>("id"));
@@ -79,30 +99,42 @@ public class CustomerFormController implements Initializable {
 
         ObservableList<Customer> customerObservableList = FXCollections.observableArrayList();
 
-        Connection connection = DBConnection.getInstance().getConnection();
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM customer");
-        while (resultSet.next()){
-            customerObservableList.add(
-                    new Customer(
-                            resultSet.getString(1),
-                            resultSet.getString(2),
-                            resultSet.getString(3),
-                            resultSet.getDouble(4)
-                    )
-            );
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM customer");
+                while (resultSet.next()) {
+                    customerObservableList.add(
+                            new Customer(
+
+                                    resultSet.getString(1),
+                                    resultSet.getString(2),
+                                    resultSet.getString(3),
+                                    resultSet.getDouble(4)
+                            )
+                    );
+                }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
+        tblCustomers.setItems(customerObservableList);
 
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        try {
-            loadTable();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+
+        loadTable();
+
+        tblCustomers.getSelectionModel().selectedItemProperty().addListener((observableValue, oldvalue, newValue) -> {
+            if (newValue != null){
+                setTextToValues((Customer) newValue);
+            }
+        });
     }
+
+
 }
 
 
